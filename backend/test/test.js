13115211c1a6e70571a5ab25849b79358e105678
sarthakag-dev <sarthak.agarwal@innostax.com>
2023-07-04 14:57:09 +0530

    Changes Test Code

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,27 +1,24 @@
-// Importing mocha and chai
 const mocha = require("mocha");
 const chai = require("chai");
 
-// Importing todoUtils.js where our code is written
 const todoUtils = require("../utils/todoutils");
 
 const expect = chai.expect;
 
 // Group of tests using describe
 describe("todoUtils", function () {
-  // We will describe each single test using it
   it("Expected To not create a TODO", () => {
-    let fare = todoUtils.addTodo("this is title");
-    expect(fare).to.equal(false);
+    let todo = todoUtils.addTodo("this is title");
+    expect(todo).to.equal(false);
   });
 
   it("Expected to not create a TODO, Invalid Prameters", () => {
-    let fare = todoUtils.addTodo("This is tile", "This is subtitle");
-    expect(fare).to.equal(false);
+    let todo = todoUtils.addTodo("This is tile", "This is subtitle");
+    expect(todo).to.equal(false);
   });
 
   it("Expected To Create a TODO", () => {
-    let fare = todoUtils.addTodo("Title", "Subtitle", "Contenet ");
-    expect(fare).to.equal(true);
+    let todo = todoUtils.addTodo("Title", "Subtitle", "Contenet ");
+    expect(todo).to.equal(true);
   });
 });
